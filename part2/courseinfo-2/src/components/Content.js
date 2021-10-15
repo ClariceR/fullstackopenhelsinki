1fef726122a81755be0c158b52baed9efd1ab2ea
clarice_r <claricerdallorto@gmail.com>
2021-10-15 15:40:52 +0100

    Completed exercise 2.1 step6

--- part2/courseinfo-2/src/components/Content.js
+++ part2/courseinfo-2/src/components/Content.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Part from "./Part";
 
-function Content({eachCourse}) {
-    console.log('eachCourse:', eachCourse)
+function Content({ eachCourse }) {
+  console.log("eachCourse:", eachCourse);
   return (
     <div>
-      <Part name={eachCourse[0].name} exercise={eachCourse[0].exercises}/>
+      <ul>
+        {eachCourse.map((part) => (
+          <Part key={part.id} name={part.name} exercise={part.exercises} />
+        ))}
+      </ul>
     </div>
   );
 }
