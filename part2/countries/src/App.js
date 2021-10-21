bdab2658c87077fda73992773fd7188d8620d0f1
clarice_r <claricerdallorto@gmail.com>
2021-10-21 12:57:31 +0100

    Can filter and show results

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,6 +2,23 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+function Countries({ countries, toSearch }) {
+  const findCountry = (country) => {
+    console.log(country)
+    if (country.name.official.toLowerCase().includes(toSearch.toLowerCase())) {
+      return country;
+    }
+  };
+  const filteredList = countries.filter(findCountry);
+  return (
+    <ul>
+      {filteredList.map((country) => (
+        <li key={country.name.official}>{country.name.official}</li>
+      ))}
+    </ul>
+  );
+}
+
 function App() {
   const [countries, setCountries] = useState([]);
   const [toSearch, setToSearch] = useState("");
@@ -12,14 +29,15 @@ function App() {
       .then((response) => setCountries(response.data));
   }, []);
 
-  const handleChange = (e) => setToSearch(e.target.value);
+  const handleSearch = (e) => setToSearch(e.target.value);
 
   return (
     <div className="App">
       <form>
         <p>Search for countries</p>
-        <input value={toSearch} onChange={handleChange} />
+        <input value={toSearch} onChange={handleSearch} />
       </form>
+      <Countries countries={countries} toSearch={toSearch} />
     </div>
   );
 }
