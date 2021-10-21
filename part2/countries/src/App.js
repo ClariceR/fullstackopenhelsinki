087dd7874dfd1320acaeea8f2ee061490aa866f2
clarice_r <claricerdallorto@gmail.com>
2021-10-21 12:26:22 +0100

    Setup axios and get data from api

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [toSearch, setToSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => setCountries(response.data));
+  }, []);
+
+  const handleChange = (e) => setToSearch(e.target.value);
+
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <form>
+        <p>Search for countries</p>
+        <input value={toSearch} onChange={handleChange} />
+      </form>
     </div>
   );
 }
